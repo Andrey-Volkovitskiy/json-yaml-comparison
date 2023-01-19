05eede3e7bfb198b92f4ebe0882c0f7746ae0505
Andrey <volk000@gmail.com>
2023-01-19 06:40:42 +0600

    Use f-strings in tests

--- tests/test_e2e_flat.py
+++ tests/test_e2e_flat.py
@@ -9,15 +9,15 @@ FILE2 = 'tests/fixtures/file2'
 
 
 @pytest.mark.parametrize("file1, file2, expected", [
-    (FILE0 + '.json', FILE0 + '.json', '{}'),
-    (FILE0 + '.yml', FILE0 + '.yaml', '{}')])
+    (f"{FILE0}.json", f"{FILE0}.json", '{}'),
+    (f"{FILE0}.yml", f"{FILE0}.yaml", '{}')])
 def test_with_empty(file1, file2, expected):
     assert generate_diff(file1, file2) == expected
 
 
 @pytest.mark.parametrize("file1, file2, expected", [
-    (FILE0 + '.json', FILE2 + '.json', 'result_half_empty.txt'),
-    (FILE0 + '.yml', FILE2 + '.yaml', 'result_half_empty.txt')])
+    (f'{FILE0}.json', f'{FILE2}.json', 'result_half_empty.txt'),
+    (f'{FILE0}.yml', f'{FILE2}.yaml', 'result_half_empty.txt')])
 def test_with_half_empty(file1, file2, expected):
     with open(PATH + expected, 'r') as expected_file:
         expected_result = expected_file.read()
@@ -25,8 +25,8 @@ def test_with_half_empty(file1, file2, expected):
 
 
 @pytest.mark.parametrize("file1, file2, expected", [
-    (FILE1 + '.json', FILE2 + '.json', 'result_flat.txt'),
-    (FILE1 + '.yaml', FILE2 + '.yaml', 'result_flat.txt')])
+    (f'{FILE1}.json', f'{FILE2}.json', 'result_flat.txt'),
+    (f'{FILE1}.yaml', f'{FILE2}.yaml', 'result_flat.txt')])
 def test_with_flat(file1, file2, expected):
     with open(PATH + expected, 'r') as expected_file:
         expected_result = expected_file.read()
